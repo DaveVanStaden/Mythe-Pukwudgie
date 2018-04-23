9cdb4ee9b08fed86b0cc84d24eab064f459951f9
23448 <23448@ma-web.nl>
2018-04-23 10:59:16 +0200

    Fixed collision for full build

--- Mythe/Assets/Scripts/DragManager.cs
+++ Mythe/Assets/Scripts/DragManager.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class DragManager : MonoBehaviour
 {
 
-    private Camera _camera;
-    private GameObject _dragObject;
-
-
+	private bool _isDragged;
     private int _hand = 0;
-    private float PickUpCooldown = 3;
+    private float _pickUpCooldown = 3;
     [SerializeField]
     private Material _material;
+	private Material _defaultMaterial;
+
 
     // Use this for initialization
     void Start()
@@ -23,7 +22,8 @@ public class DragManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		Debug.Log (_hand);
+		//Debug.Log (_hand);
+		Debug.Log(_hand);
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
 		if (Physics.Raycast(ray, out hit))
@@ -32,22 +32,31 @@ public class DragManager : MonoBehaviour
 			{
 				Rigidbody _rb = hit.collider.gameObject.GetComponent<Rigidbody>();
 				_hand = _hand + 1;
-				if (_hand == 1) {
-					PickUpCooldown -= Time.deltaTime;
+				_isDragged = true;
+				if (_hand == 1 && _isDragged == true) {
+					_pickUpCooldown -= Time.deltaTime;
 					hit.collider.gameObject.transform.parent = this.transform;
 					hit.collider.gameObject.transform.position = transform.position;
+					_defaultMaterial = hit.collider.gameObject.GetComponent<Renderer> ().material;
+					hit.collider.gameObject.GetComponent<Renderer> ().material = _material;
+                    Physics.IgnoreCollision(hit.collider, this.GetComponent<Collider>(), true);
+					//hit.collider.gameObject.GetComponent<Renderer> ().material.color.a = ;
+					//hit.collider.gameObject.tag("Drag").GetComponent<Renderer>().material = _material;
 					_rb.useGravity = false;
-					if (_hand == 1 && Input.GetKeyDown ("e") && PickUpCooldown < 0) {
-						_hand = _hand - 1;
+					if (_hand == 1 && Input.GetKeyDown ("e") && _pickUpCooldown < 0 && _isDragged == true) {
+						_hand = 1 - 1;
+						_isDragged = false;
 					}
 				} else if (_hand != 1) {
-					_rb.useGravity = true;
-					_material = hit.collider.gameObject.GetComponent<Renderer> ().material;
+					hit.collider.gameObject.GetComponent<Renderer> ().material = _defaultMaterial;
+                    Physics.IgnoreCollision(hit.collider, this.GetComponent<Collider>(), false);
+                    _rb.useGravity = true;
 					hit.collider.gameObject.transform.position = hit.collider.gameObject.transform.position;
 					hit.collider.gameObject.transform.parent = null;
-				} else if (_hand == 1 && Input.GetKeyDown ("e")) {
-					_hand = _hand + 0;
-				
+					_isDragged = false;
+				} if (_hand == 1 && Input.GetKeyDown ("e") && _isDragged == true || _hand > 1) {
+					_hand = 0 - 1;
+					_isDragged = false;
 				}
 			}
 
